6bde42b05a43fd1e8d79746f199a441517418ded
Skitou <chiantigally@gmail.com>
2023-08-09 18:06:08 +0200

    Added callback

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-slint::slint!(import { App } from "ui/main.slint";);
+slint::include_modules!();
 fn main() {
-    let app = App::new().unwrap();
-    app.run().unwrap();
+    slint::init_translations!(std::env::current_exe().unwrap().parent().unwrap().join("lang"));
+
+    let app_window: App = App::new().unwrap();
+    app_window.set_logs(slint::SharedString::from("TEST LOGS:\n"));
+
+    let app_window_weak = app_window.as_weak();
+    app_window.global::<BtnLogic>().on_btn_compile(move || {
+        println!("Clicked!")
+    });
+
+    app_window.run().unwrap();
 }
