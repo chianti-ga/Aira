1d491e7448347c9a4ccbf4be72e3d99b143e6e28
Skitou <chiantigally@gmail.com>
2023-08-12 23:05:46 +0200

    Fixed out-of-scope borrowing of App

--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,27 @@ fn main() {
     slint::init_translations!(std::env::current_exe().unwrap().parent().unwrap().join("lang"));
 
     let app_window:App = App::new().unwrap();
+    //let app_window_weak:Weak<App> = app_window.as_weak();
     app_window.global::<TextLogic>().set_logs(slint::SharedString::from("TEST LOGS:\n"));
 
     set_btn_actions(&app_window);
 
+
     app_window.run().unwrap();
 }
-fn set_btn_actions(app:&'static App) {
-    let btn_logic:BtnLogic = app.global::<BtnLogic>();
-    let path_logic:FilesPathsLogic = app.global::<FilesPathsLogic>();
+
+fn set_btn_actions(app:&App){
+    let btn_logic: BtnLogic = app.global::<BtnLogic>();
+
+    let app_weak = app.as_weak().unwrap();
 
     btn_logic.on_btn_compile(move || {
-        let qcinf = PropsData::default();
-        create_qc_props(qcinf)
+        let qc_inf = PropsData::default();
+        create_qc_props(qc_inf);
     });
 
     btn_logic.on_btn_gmod_bin_selection(move || {
-        let dir:Option<PathBuf> = FileDialog::new()
-            .pick_folder();
-        match dir {
-            Some(path) => {path_logic.set_gmod_bin_path(SharedString::from(path.as_path().to_str().get_or_insert("INVALID STR").to_string()))}
-            None => {path_logic.set_gmod_bin_path(SharedString::from("INVALID"))}
-        }
+        let dir: Option<PathBuf> = FileDialog::new().pick_folder();
+        app_weak.global::<FilesPathsLogic>().set_gmod_bin_path(SharedString::from(dir.unwrap().to_str().unwrap()));
     });
 }
