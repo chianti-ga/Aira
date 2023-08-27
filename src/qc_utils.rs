49ca3e5d1157aa99efb6f8203b5ee73cbadd345c
Skitou <chiantigally@gmail.com>
2023-08-27 23:55:54 +0200

    Reworked PropsData struc and CollisionModel.
    Added display implementation to serialize to .qc

--- src/qc_utils.rs
+++ src/qc_utils.rs
@@ -1,51 +1,78 @@
+use std::fmt::{Display, Formatter, write};
+
 const CBOX_DEFAULT: [f32; 6] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
 
 pub struct PropsData {
     pub modelname: String,
-    pub bodygroup: String,
+    pub body: String,
     pub surfaceprop: String,
     pub contents: String,
     pub cdmaterials: String,
+    pub texturegroup:Vec<String>,
     pub cbox: [f32; 6],
-    pub collisionmodel: String,
+    pub collisionmodel: CollisionModel,
+}
+
+pub struct CollisionModel {
+    pub automass: bool,
+    pub mass: f32,
+    pub concave: bool,
 }
 
 impl Default for PropsData {
     fn default() -> PropsData {
         PropsData {
             modelname: "".to_string(),
-            bodygroup: "".to_string(),
-            surfaceprop: "".to_string(),
-            contents: "".to_string(),
+            body: "".to_string(),
+            surfaceprop: "default".to_string(),
+            contents: "solid".to_string(),
             cdmaterials: "".to_string(),
+            texturegroup: Vec::new(),
             cbox: CBOX_DEFAULT,
-            collisionmodel: "".to_string(),
+            collisionmodel: CollisionModel{
+                automass: true,
+                mass: 0.0,
+                concave: true,
+            },
         }
     }
 }
 
-impl ToString for PropsData {
-    fn to_string(&self) -> String {
-        format!(
-            "modelname:{}\n\
-        bodygroup:{}\n\
-        surfaceprop:{}\n\
-        contents:{}\n\
-        cdmaterials:{}\n\
-        cbox:{:?}\n\
-        collisionmodel{}",
-            self.modelname,
-            self.bodygroup,
-            self.surfaceprop,
-            self.contents,
-            self.cdmaterials,
-            self.cbox,
-            self.collisionmodel
-        )
+impl Display for PropsData {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "\"$modelname\" \"{}\"", self.modelname)?;
+
+        writeln!(f, "\"$body\" \"{}\"", self.body)?;
+
+        writeln!(f, "\"$surfaceprop\" \"{}\"", self.surfaceprop)?;
+
+        writeln!(f, "\"$contents\" \"{}\"", self.contents)?;
+
+        write!(f, "\"$texturegroup\" \"skins\"\n{{")?;
+
+        self.texturegroup.iter().for_each(|texture| {
+           write!(f, " {{ {} }}", texture).unwrap();
+        });
+        writeln!(f, "}}")?;
+        write!(f, "\"cbox\"")?;
+        self.cbox.iter().for_each(|coord| {
+             write!(f, " {} ", coord).unwrap()
+        });
+        write!(f, "\n").unwrap()
     }
 }
 
-pub fn create_qc_props(data: PropsData) {
-    //Debug
-    println!("{}", data.to_string());
+impl Display for CollisionModel{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "\"$collisionmodel\" {{\n ")?;
+        if self.automass {
+            writeln!(f, "\"$automass\"")?;
+        }else {
+            writeln!(f, "\"{}\"", self.mass)?;
+        }
+        if self.concave {
+            writeln!(f, "\"$concave\"")?;
+        }
+        write!(f,"}}")
+    }
 }
