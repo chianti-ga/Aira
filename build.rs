03976441f5e9727b19273247d9dfced8005f3ec9
Skitou <chiantigally@gmail.com>
2023-08-28 05:29:11 +0200

    Fixed logo on windows

--- build.rs
+++ build.rs
@@ -2,9 +2,10 @@ fn main() {
     //let compiler_config:CompilerConfiguration =CompilerConfiguration::default();
     slint_build::compile("ui/main.slint").unwrap();
     slint_build::print_rustc_flags().unwrap();
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("logo.ico");
+
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("ui/logo.ico");
         res.compile().unwrap();
     }
 }
