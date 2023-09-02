45821769eb1554d00f9103d706a0d7bcecf77b07
Skitou <chiantigally@gmail.com>
2023-09-02 13:30:05 +0200

    Finished studio_mdl compile function

--- src/tools_utils.rs
+++ src/tools_utils.rs
@@ -1,3 +1,4 @@
+use std::thread;
 use std::collections::HashSet;
 use std::fs::{copy, create_dir_all, File, remove_dir_all, remove_file};
 use std::io::Write;
@@ -5,7 +6,6 @@ use std::path::{Path, PathBuf};
 use std::process::{Command, Output, Stdio};
 use std::sync::{mpsc, Mutex, MutexGuard};
 use std::sync::mpsc::{Receiver, Sender};
-use std::thread;
 
 use lazy_static::lazy_static;
 use log::{debug, error, info};
@@ -17,10 +17,10 @@ use serde::{Deserialize, Serialize};
 use slint::{ComponentHandle, SharedString, Weak};
 use walkdir::WalkDir;
 
-use crate::{App, TextLogic};
+use crate::{App, TextLogic, TOOLS_PATHS};
 
 lazy_static! {
-    static ref BASE_NAMES: Mutex<HashSet<String>> = Mutex::new(HashSet::new());
+    pub static ref BASE_NAMES: Mutex<HashSet<String>> = Mutex::new(HashSet::new());
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -39,6 +39,7 @@ pub fn vtex_compile(app_weak: Weak<App>, out_path: &Path, materials_path: &Path,
             app_weak.upgrade_in_event_loop(move |app| {
                 let mut logs: SharedString = app.global::<TextLogic>().get_logs();
                 logs.push_str("Cleaning up output folder...\n");
+                //app.global::<TextLogic>().set_logs();
                 app.global::<TextLogic>().set_logs(logs);
             }).unwrap();
         }
@@ -82,7 +83,7 @@ pub fn vtex_compile(app_weak: Weak<App>, out_path: &Path, materials_path: &Path,
             let entry_path: String = entry.path().to_string_lossy().to_string();
             let final_filename: String = entry_path.replace(materials_path.to_str().unwrap().trim_start_matches(std::path::MAIN_SEPARATOR), "").replace(std::path::MAIN_SEPARATOR, "_").trim_start_matches('_').to_lowercase();
 
-            let base_name: String = final_filename.replace(entry.file_name().to_str().unwrap(), "");
+            let base_name: String = final_filename.replace(entry.file_name().to_str().unwrap(), "").trim_end_matches('_').to_string();
             BASE_NAMES.lock().unwrap().insert(base_name);
 
             let final_file: PathBuf = materials_out.join(final_filename.as_str());
@@ -95,7 +96,7 @@ pub fn vtex_compile(app_weak: Weak<App>, out_path: &Path, materials_path: &Path,
                 app.global::<TextLogic>().set_logs(logs);
             }).unwrap();
 
-            textures_files.push(final_filename.clone().trim_end_matches('_').to_string());
+            textures_files.push(final_filename.clone());
         }
     });
 
@@ -107,12 +108,10 @@ pub fn vtex_compile(app_weak: Weak<App>, out_path: &Path, materials_path: &Path,
             }
             app_weak.upgrade_in_event_loop(move |app| {
                 let mut logs: SharedString = app.global::<TextLogic>().get_logs();
-                logs.push_str(msg.as_str().trim_end_matches("\n"));
-                logs.push_str("\n");
+                logs.push_str(msg.replace('\n', "").as_str());
+                //logs.push_str("\n");
                 app.global::<TextLogic>().set_logs(logs);
             }).unwrap();
-
-            debug!("bite")
         }
     });
 
@@ -154,9 +153,14 @@ pub fn vtex_compile(app_weak: Weak<App>, out_path: &Path, materials_path: &Path,
 }
 
 pub fn vmt_generate(app_weak: Weak<App>, out_path: &Path) {
-    let materials_out: PathBuf = out_path.join("materials/");
+    app_weak.upgrade_in_event_loop(move |app| {
+        let mut logs: SharedString = app.global::<TextLogic>().get_logs();
+        logs.push_str("Generating VMT files...");
+        app.global::<TextLogic>().set_logs(logs);
+    }).unwrap();
 
-    let mut base_names: MutexGuard<HashSet<String>> = BASE_NAMES.lock().unwrap();
+    let materials_out: PathBuf = out_path.join("materials/");
+    let base_names: MutexGuard<HashSet<String>> = BASE_NAMES.lock().unwrap();
     base_names.par_iter().for_each(|base_name| {
         let mut base_color: String = base_name.clone();
         base_color.push_str("_basecolor");
@@ -206,16 +210,76 @@ pub fn vmt_generate(app_weak: Weak<App>, out_path: &Path) {
             }
         }
     });
+
+    app_weak.upgrade_in_event_loop(move |app| {
+        let mut logs: SharedString = app.global::<TextLogic>().get_logs();
+        logs.push_str("VMT files generated!");
+        app.global::<TextLogic>().set_logs(logs);
+    }).unwrap();
 }
 
 // https://developer.valvesoftware.com/wiki/StudioMDL_(Source_1)
-pub fn studiomdl_compile(app_weak: Weak<App>, game_path: &Path, qc_file_path: &Path, tools_paths: ToolsPaths) {
+pub fn studiomdl_compile(app_weak: Weak<App>, out_path: &Path, model_path: SharedString) {
+    debug!("{}",     out_path.join("qc.qc").to_str().unwrap());
+    let tools_paths = TOOLS_PATHS.lock().unwrap();
     let studio_mdl: &Path = tools_paths.studio_mdl.as_path();
+    let game_path = tools_paths.gmad.parent().unwrap().parent().unwrap().join("garrysmod");
+    debug!("{}", game_path.to_string_lossy());
 
-    let studio_mdl_cmd: &mut Command = Command::new(studio_mdl).args(["-game", game_path.to_str().unwrap(), "-nop4", "-verbose", qc_file_path.to_str().unwrap()]);
-    cleanup();
-}
+    let mut studio_mdl_cmd: Command = Command::new(studio_mdl);
+    studio_mdl_cmd.args(["-game", game_path.to_str().unwrap(), "-nop4", "-verbose", out_path.join("qc.qc").to_str().unwrap()]);
+    studio_mdl_cmd.current_dir(out_path);
 
-fn cleanup(){
+    let studio_mdl_stdout = studio_mdl_cmd.spawn().unwrap().wait_with_output().unwrap().stdout;
+
+    println!("{}", String::from_utf8(studio_mdl_stdout.clone()).unwrap().as_str());
+
+    app_weak.upgrade_in_event_loop(move |app| {
+        let mut logs: SharedString = app.global::<TextLogic>().get_logs();
+        logs.push_str(String::from_utf8(studio_mdl_stdout).unwrap().as_str());
+        app.global::<TextLogic>().set_logs(logs);
+    }).unwrap();
+
+    let compile_out = game_path.clone().join("models").join("aira_temp");
+    let model_path = Path::new(model_path.as_str());
+    let name = model_path.file_name().unwrap().to_string_lossy();
+
+    WalkDir::new(compile_out).into_iter().filter_map(|e| e.ok()).for_each(|entry| {
+        println!("{:?}", entry.path());
+        println!("{:?}", out_path.join(name.trim_end_matches(".smd")));
+        match create_dir_all(out_path.join(name.trim_end_matches(".smd"))) {
+            Ok(_) => {}
+            Err(err) => {
+                error!("{:?}", err);
+                MessageDialog::new()
+                    .set_title("Error")
+                    .set_level(MessageLevel::Error)
+                    .set_description(&format!("Cannot create models output folder :, {}", err))
+                    .show();
+            }
+        };
+        match copy(entry.path(), out_path.join(name.trim_end_matches(".smd"))) {
+            Ok(_) => {}
+            Err(err) => {
+                error!("{:?}", err);
+                MessageDialog::new()
+                    .set_title("Error")
+                    .set_level(MessageLevel::Error)
+                    .set_description(&format!("Cannot copy compiled model to output folder :, {}", err))
+                    .show();
+            }
+        }
+        match remove_file(entry.path()) {
+            Ok(_) => {}
+            Err(err) => {
+                error!("{:?}", err);
+                MessageDialog::new()
+                    .set_title("Error")
+                    .set_level(MessageLevel::Error)
+                    .set_description(&format!("Cannot delete temp folder :, {}", err))
+                    .show();
+            }
+        }
+    });
     BASE_NAMES.lock().unwrap().clear();
 }
