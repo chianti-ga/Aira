4f78baa7792b6d28d738a12ad68e593316ef7d84
Skitou <chiantigally@gmail.com>
2023-09-02 13:29:24 +0200

    Fixed serialisation

--- src/qc.rs
+++ src/qc.rs
@@ -1,77 +1,74 @@
 use std::fmt::{Display, Formatter};
 
-const CBOX_DEFAULT: [f32; 6] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+pub const CBOX_DEFAULT: &str = "0 0 0 0 0 0";
 
+#[derive(Clone)]
 pub struct PropsData {
     pub modelname: String,
     pub body: String,
     pub surfaceprop: String,
     pub contents: String,
     pub cdmaterials: String,
+    pub sequence: String,
     pub texturegroup: Vec<String>,
-    pub cbox: [f32; 6],
+    pub cbox: String,
     pub collisionmodel: CollisionModel,
 }
 
+#[derive(Clone)]
 pub struct CollisionModel {
+    pub modelname: String,
     pub automass: bool,
-    pub mass: f32,
+    pub mass: String,
     pub concave: bool,
 }
 
-impl Default for PropsData {
-    fn default() -> PropsData {
-        PropsData {
+impl Default for CollisionModel {
+    fn default() -> CollisionModel {
+        CollisionModel {
             modelname: "".to_string(),
-            body: "".to_string(),
-            surfaceprop: "default".to_string(),
-            contents: "solid".to_string(),
-            cdmaterials: "".to_string(),
-            texturegroup: Vec::new(),
-            cbox: CBOX_DEFAULT,
-            collisionmodel: CollisionModel {
-                automass: true,
-                mass: 0.0,
-                concave: true,
-            },
+            automass: true,
+            mass: "0.0".to_string(),
+            concave: true,
         }
     }
 }
 
 impl Display for PropsData {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "\"$modelname\" \"{}\"", self.modelname)?;
+        writeln!(f, "$modelname \"{}\"", self.modelname.replace("smd", "mdl"))?;
 
-        writeln!(f, "\"$body\" \"{}\"", self.body)?;
+        writeln!(f, "$body {} \"{}\"", self.modelname.replace(".smd", ""), self.body)?;
 
-        writeln!(f, "\"$surfaceprop\" \"{}\"", self.surfaceprop)?;
+        writeln!(f, "$surfaceprop \"{}\"", self.surfaceprop)?;
 
-        writeln!(f, "\"$contents\" \"{}\"", self.contents)?;
+        writeln!(f, "$contents \"{}\"", self.contents)?;
 
-        write!(f, "\"$texturegroup\" \"skins\"\n{{")?;
+        writeln!(f, "$cdmaterials \"{}\"", self.cdmaterials)?;
+
+        writeln!(f, "$sequence idle \"{}\"", self.sequence)?;
+
+        write!(f, "$texturegroup skins\n{{")?;
 
         self.texturegroup.iter().for_each(|texture| {
             write!(f, " {{ {} }}", texture).unwrap();
         });
         writeln!(f, "}}")?;
-        write!(f, "\"cbox\"")?;
-        self.cbox.iter().for_each(|coord| {
-            write!(f, " {} ", coord).unwrap()
-        });
+        writeln!(f, "$cbox {}", self.cbox)?;
         writeln!(f, "{}", self.collisionmodel)
     }
 }
 
 impl Display for CollisionModel {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "\"$collisionmodel\" {{\n ")?;
+        write!(f, "$collisionmodel \"{}\" {{\n ", self.modelname)?;
         if self.automass {
-            writeln!(f, "\"$automass\"")?;
+            writeln!(f, "$automass")?;
         } else {
-            writeln!(f, "\"{}\"", self.mass)?;
+            writeln!(f, "$mass {}", self.mass)?;
         }
         if self.concave {
-            writeln!(f, "\"$concave\"")?;
+            writeln!(f, "$concave")?;
         }
         write!(f, "}}")
     }
